a7493d385a0d5f58cbc6b8e74a6501365a2d5c11
Cintra <gui7lk@gmail.com>
2022-09-15 16:02:16 -0300

    posts e sidebar totalmente componentizados

--- src/Posts.js
+++ src/Posts.js
@@ -53,126 +53,119 @@ function Comment(props) {
     }
 }
 
-function Post(props) {
-
-    return (
-        <div class="Post">
-            <div class="Post-user">
-                <div>
-                    <div class="User-photo">
-                        <div class="Gradient-circle Off"></div>
-                        <img class="Only" src={props.userImg} alt={props.userName} />
-                    </div>
-
-                    <p>{props.userName}</p>
-                </div>
-
-                <div class="Post-options">
-                    <p>...</p>
-                </div>
-            </div>
-
-            <div class="Post-img">
-                <img src={props.postImg} alt="Imagem de post" />
-            </div>
-
-            <div class="Post-interactions">
-                <div>
-                    <div class="Like-interaction">
-                        <ion-icon id="L1" name="heart-outline"
-                            onClick="Like(document.getElementById('L1'), document.getElementById('HL1'))">
-                        </ion-icon>
-                        <div id="HL1" class="Liked Off"
-                            onClick="Like(document.getElementById('L1'), document.getElementById('HL1'))"></div>
-                    </div>
-                    <ion-icon class="Chat-icon" name="chatbubble-outline"></ion-icon>
-                    <ion-icon name="paper-plane-outline"></ion-icon>
-                </div>
-                <ion-icon name="bookmark-outline"></ion-icon>
-            </div>
-
-            <div class="Post-informations">
-                <div class="Likes">
-                    <img src={props.userLikedImg} alt="Curtiu a publicação" />
-                    <p> Curtido por <strong class="Liked-by">{props.userLiked}</strong> e <strong>outras
-                        pessoas</strong></p>
-                </div>
-
-                <div class="Post-caption">
-                    <p><strong>{props.userName}</strong> {props.postCaption}</p>
-                </div>
-
-                <Comment
-                    comments={props.comments}
-                    userComment1={props.userComment1}
-                    comment1={props.comment1}
-                    userComment2={props.userComment2}
-                    comment2={props.comment2}
-                />
-
-                <div class="Post-time">
-                    <span>Há {props.time}</span>
-                </div>
-            </div>
-
-            <div class="Write-comment">
-                <ion-icon name="happy-outline"></ion-icon>
-
-                <input type="text" name="" id="" placeholder="Adicione um comentário..." />
-
-                <button>Publicar</button>
-            </div>
-        </div>
-    )
-}
-
 export default function Posts() {
+    const data = [{
+        userImg: "images/Users-img/meowed.svg",
+        userName: "meowed",
+        postImg: "images/Posts/gato-telefone.svg",
+        userLikedImg: "images/Users-img/User-photo.jpg",
+        userLiked: "Guicintra27",
+        postCaption: "vendo um miauflix",
+        comments: "12",
+        userComment1: "Guicintra27",
+        comment1: "melhor forma 😋",
+        userComment2: "Hgata",
+        comment2: "só faltou htinha junto 👀",
+        time: "6 horas"
+    }, {
+        userImg: "images/Users-img/barked.svg",
+        userName: "barked",
+        postImg: "images/Posts/dog.svg",
+        userLikedImg: "images/Users-img/meowed.svg",
+        userLiked: "meowed",
+        postCaption: "soninho da beleza",
+        comments: "10",
+        userComment1: "meowed",
+        comment1: "vou tirar um soninho desse pra ficar bonito assim 😝",
+        userComment2: "",
+        comment2: "",
+        time: "13 horas"
+    }, {
+        userImg: "images/Users-img/User-photo.jpg",
+        userName: "Guicintra27",
+        postImg: "https://cdn1.mundodastribos.com/458820-Fotos-de-cachorros-fantasiados-11.jpg",
+        userLikedImg: "images/Users-img/9gag 1.svg",
+        userLiked: "9gag",
+        postCaption: "meu dogpresley",
+        comments: "15",
+        userComment1: "",
+        comment1: "",
+        userComment2: "",
+        comment2: "",
+        time: "1 dia"
+    }];
+    
     return (
         <div>
-            <Post
-                userImg="images/Users-img/meowed.svg"
-                userName="meowed"
-                postImg="images/Posts/gato-telefone.svg"
-                userLikedImg="images/Users-img/User-photo.jpg"
-                userLiked="Guicintra27"
-                postCaption="vendo um miauflix"
-                comments="12"
-                userComment1="Guicintra27"
-                comment1="melhor forma 😋"
-                userComment2="Hgata"
-                comment2="só faltou htinha junto 👀"
-                time="6 horas"
-            />
+            {data.map((item) =>
+                <div class="Post">
+                    <div class="Post-user">
+                        <div>
+                            <div class="User-photo">
+                                <div class="Gradient-circle Off"></div>
+                                <img class="Only" src={item.userImg} alt={item.userName} />
+                            </div>
 
-            <Post
-                userImg="images/Users-img/barked.svg"
-                userName="barked"
-                postImg="images/Posts/dog.svg"
-                userLikedImg="images/Users-img/meowed.svg"
-                userLiked="meowed"
-                postCaption="soninho da beleza"
-                comments="10"
-                userComment1="meowed"
-                comment1="vou tirar um soninho desse pra ficar bonito assim 😝"
-                userComment2=""
-                comment2=""
-                time="13 horas"
-            />
+                            <p>{item.userName}</p>
+                        </div>
 
-            <Post
-                userImg="images/Users-img/User-photo.jpg"
-                userName="Guicintra27"
-                postImg="https://cdn1.mundodastribos.com/458820-Fotos-de-cachorros-fantasiados-11.jpg"
-                userLikedImg="images/Users-img/9gag 1.svg"
-                userLiked="9gag"
-                postCaption="meu dogpresley"
-                comments="15"
-                userComment1=""
-                comment1=""
-                userComment2=""
-                comment2=""
-                time="1 dia"
-            />
+                        <div class="Post-options">
+                            <p>...</p>
+                        </div>
+                    </div>
+
+                    <div class="Post-img">
+                        <img src={item.postImg} alt="Imagem de post" />
+                    </div>
+
+                    <div class="Post-interactions">
+                        <div>
+                            <div class="Like-interaction">
+                                <ion-icon id="L1" name="heart-outline"
+                                    onClick="Like(document.getElementById('L1'), document.getElementById('HL1'))">
+                                </ion-icon>
+                                <div id="HL1" class="Liked Off"
+                                    onClick="Like(document.getElementById('L1'), document.getElementById('HL1'))"></div>
+                            </div>
+                            <ion-icon class="Chat-icon" name="chatbubble-outline"></ion-icon>
+                            <ion-icon name="paper-plane-outline"></ion-icon>
+                        </div>
+                        <ion-icon name="bookmark-outline"></ion-icon>
+                    </div>
+
+                    <div class="Post-informations">
+                        <div class="Likes">
+                            <img src={item.userLikedImg} alt="Curtiu a publicação" />
+                            <p> Curtido por <strong class="Liked-by">{item.userLiked}</strong> e <strong>outras
+                                pessoas</strong></p>
+                        </div>
+
+                        <div class="Post-caption">
+                            <p><strong>{item.userName}</strong> {item.postCaption}</p>
+                        </div>
+
+                        <Comment
+                            comments={item.comments}
+                            userComment1={item.userComment1}
+                            comment1={item.comment1}
+                            userComment2={item.userComment2}
+                            comment2={item.comment2}
+                        />
+
+                        <div class="Post-time">
+                            <span>Há {item.time}</span>
+                        </div>
+                    </div>
+
+                    <div class="Write-comment">
+                        <ion-icon name="happy-outline"></ion-icon>
+
+                        <input type="text" name="" id="" placeholder="Adicione um comentário..." />
+
+                        <button>Publicar</button>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
--- src/Suggestions.js
+++ src/Suggestions.js
@@ -1,23 +1,33 @@
-function SuggestedFriend(props) {
-    return (
-        <div class="Suggested-friend">
-            <div>
-                <a href={props.linkIg}>
-                    <img src={props.linkImg} alt={props.userSuggested} />
-                </a>
-                <div>
-                    <a href={props.linkIg}>
-                        <strong>{props.userSuggested}</strong>
-                    </a>
-                    <p>{props.subtitle}</p>
-                </div>
-            </div>
-            <p>Seguir</p>
-        </div>
-    )
-}
-
 export default function Suggestions() {
+    const data = [
+        {
+            linkIg: "https://www.instagram.com/alex_eubank15/",
+            linkImg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT94XZA3RhwYUaH18gZqHbLfr3AaU90FFiaxA&usqp=CAU",
+            userSuggested: "alex_eubank15",
+            subtitle: "Novo no instagram"
+        }, {
+            linkIg: "https://www.instagram.com/thew6rst/",
+            linkImg: "https://yt3.ggpht.com/ytc/AMLnZu_172Hicqkw-pzZpQDPnUNVYLaqjDUg3kt_AfduhQ=s88-c-k-c0x00ffffff-no-rj",
+            userSuggested: "thew6rst",
+            subtitle: "segue você"
+        }, {
+            linkIg: "https://www.instagram.com/leandro_karnal/",
+            linkImg: "https://pbs.twimg.com/profile_images/1399424959668162566/S4wPpTK1_400x400.jpg",
+            userSuggested: "leandro_karnal",
+            subtitle: "segue você"
+        }, {
+            linkIg: "#",
+            linkImg: "https://pbs.twimg.com/profile_images/1198910664691126272/EANypwvT_400x400.jpg",
+            userSuggested: "miauflix",
+            subtitle: "Seguido(a) por mewoed e mais..."
+        }, {
+            linkIg: "https://www.instagram.com/lf_ponde/",
+            linkImg: "https://yt3.ggpht.com/ytc/AMLnZu_gPM_XF0juZA9LFYbLP_i0QZyeidAUysjNNVujfQ=s900-c-k-c0x00ffffff-no-rj",
+            userSuggested: "lf_ponde",
+            subtitle: "Seguido(a) por filomoderna e ..."
+        }
+    ];
+    
     return (
         <div id="Sugestions">
             <div id="More-sugestions">
@@ -27,25 +37,21 @@ export default function Suggestions() {
                 </a>
             </div>
 
-            <SuggestedFriend linkIg="https://www.instagram.com/alex_eubank15/" linkImg="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT94XZA3RhwYUaH18gZqHbLfr3AaU90FFiaxA&usqp=CAU"
-                userSuggested="alex_eubank15" subtitle="Novo no instagram"
-            />
-
-            <SuggestedFriend linkIg="https://www.instagram.com/thew6rst/" linkImg="https://yt3.ggpht.com/ytc/AMLnZu_172Hicqkw-pzZpQDPnUNVYLaqjDUg3kt_AfduhQ=s88-c-k-c0x00ffffff-no-rj"
-                userSuggested="thew6rst" subtitle="segue você"
-            />
-
-            <SuggestedFriend linkIg="https://www.instagram.com/leandro_karnal/" linkImg="https://pbs.twimg.com/profile_images/1399424959668162566/S4wPpTK1_400x400.jpg"
-                userSuggested="leandro_karnal" subtitle="segue você"
-            />
-
-            <SuggestedFriend linkIg="#" linkImg="https://pbs.twimg.com/profile_images/1198910664691126272/EANypwvT_400x400.jpg"
-                userSuggested="miauflix" subtitle="Seguido(a) por mewoed e mais..."
-            />
-
-            <SuggestedFriend linkIg="https://www.instagram.com/lf_ponde/" linkImg="https://yt3.ggpht.com/ytc/AMLnZu_gPM_XF0juZA9LFYbLP_i0QZyeidAUysjNNVujfQ=s900-c-k-c0x00ffffff-no-rj"
-                userSuggested="lf_ponde" subtitle="Seguido(a) por filomoderna e ..."
-            />
+            {data.map((item) =>
+                <div class="Suggested-friend">
+                    <div>
+                        <a href={item.linkIg}>
+                            <img src={item.linkImg} alt={item.userSuggested} />
+                        </a>
+                        <div>
+                            <a href={item.linkIg}>
+                                <strong>{item.userSuggested}</strong>
+                            </a>
+                            <p>{item.subtitle}</p>
+                        </div>
+                    </div>
+                    <p>Seguir</p>
+                </div>)}
         </div>
     )
 }

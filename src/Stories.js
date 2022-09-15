2192334616c96fe2763d54fa008cdc23a22ec58c
Cintra <gui7lk@gmail.com>
2022-09-15 16:21:39 -0300

    Site totalmente componentizado

--- src/Stories.js
+++ src/Stories.js
@@ -1,99 +1,53 @@
-export default function Stories(){
+export default function Stories() {
+    const data = [
+        {
+            userImg: "images/Users-img/9gag 1.svg",
+            userName: "9gag"
+        }, {
+            userImg: "images/Users-img/barked.svg",
+            userName: "Barked"
+        }, {
+            userImg: "images/Users-img/filomoderna.svg",
+            userName: "filomoderna"
+        }, {
+            userImg: "images/Users-img/memeriagourmet.svg",
+            userName: "memeriag.."
+        }, {
+            userImg: "images/Users-img/nathanwpylestrangeplanet.svg",
+            userName: "nathanw.."
+        }, {
+            userImg: "images/Users-img/respondeai.svg",
+            userName: "respondeai"
+        }, {
+            userImg: "images/Users-img/wawawiwacomicsa 1.svg",
+            userName: "wawawi..."
+        }, {
+            userImg: "images/Users-img/meowed.svg",
+            userName: "meowed"
+        }, {
+            userImg: "https://i.pinimg.com/originals/53/c4/98/53c498f0edb7ccf1390429f68162eab5.jpg",
+            userName: "Neymarjr"
+        }, {
+            userImg: "https://yt3.ggpht.com/szyPYpR-sT0BiZwPndOALzhCaZM6H9xxVFHUZKhf8HCUuPGS7vcFOHKzhV3HtH7i_MqpFUVDfg=s900-c-k-c0x00ffffff-no-rj",
+            userName: "mateusas.."
+        }
+    ];
+    
     return (
         <div id="Story-box">
-                <div id="Story-button-mobile">
-                    <img src="images/Users-img/Story-button.svg" alt=""/>
-                </div>
-        
-                <div class="Story">
-                    <div class="Story-img">
-                        <div class="Gradient-circle"></div>
-                        <img src="images/Users-img/9gag 1.svg" alt="Story"/>
-                    </div>
-
-                    <p>9gag</p>
-                </div>
-
-                <div class="Story">
-                    <div class="Story-img">
-                        <div class="Gradient-circle"></div>
-                        <img src="images/Users-img/barked.svg" alt="Story"/>
-                    </div>
-
-                    <p>Barked</p>
-                </div>
-
-                <div class="Story">
-                    <div class="Story-img">
-                        <div class="Gradient-circle"></div>
-                        <img src="images/Users-img/filomoderna.svg" alt="Story"/>
-                    </div>
-
-                    <p>filomoderna</p>
-                </div>
-
-                <div class="Story">
-                    <div class="Story-img">
-                        <div class="Gradient-circle"></div>
-                        <img src="images/Users-img/memeriagourmet.svg" alt="Story"/>
-                    </div>
-
-                    <p>memeriag..</p>
-                </div>
-
-                <div class="Story">
-                    <div class="Story-img">
-                        <div class="Gradient-circle"></div>
-                        <img src="images/Users-img/nathanwpylestrangeplanet.svg" alt="Story"/>
-                    </div>
-
-                    <p>nathanw..</p>
-                </div>
-
-                <div class="Story">
-                    <div class="Story-img">
-                        <div class="Gradient-circle"></div>
-                        <img src="images/Users-img/respondeai.svg" alt="Story"/>
-                    </div>
-
-                    <p>respondeai</p>
-                </div>
-
-                <div class="Story">
-                    <div class="Story-img">
-                        <div class="Gradient-circle"></div>
-                        <img src="images/Users-img/wawawiwacomicsa 1.svg" alt="Story"/>
-                    </div>
-
-                    <p>wawawi...</p>
-                </div>
-
-                <div class="Story">
-                    <div class="Story-img">
-                        <div class="Gradient-circle"></div>
-                        <img src="images/Users-img/meowed.svg" alt="Story"/>
-                    </div>
-
-                    <p>meowed</p>
-                </div>
-
-                <div class="Story">
-                    <div class="Story-img">
-                        <div class="Gradient-circle"></div>
-                        <img src="https://i.pinimg.com/originals/53/c4/98/53c498f0edb7ccf1390429f68162eab5.jpg" alt="Story"/>
-                    </div>
-
-                    <p>Neymarjr</p>
-                </div>
-
-                <div class="Story">
-                    <div class="Story-img">
-                        <div class="Gradient-circle"></div>
-                        <img src="https://yt3.ggpht.com/szyPYpR-sT0BiZwPndOALzhCaZM6H9xxVFHUZKhf8HCUuPGS7vcFOHKzhV3HtH7i_MqpFUVDfg=s900-c-k-c0x00ffffff-no-rj" alt="Story"/>
-                    </div>
-
-                    <p>mateusas..</p>
-                </div>
+            <div id="Story-button-mobile">
+                <img src="images/Users-img/Story-button.svg" alt="" />
             </div>
+
+            {data.map((item) =>
+                <div class="Story">
+                    <div class="Story-img">
+                        <div class="Gradient-circle"></div>
+                        <img src={item.userImg} alt={item.userName} />
+                    </div>
+
+                    <p>{item.userName}</p>
+                </div>)}
+        </div>
     )
 }
